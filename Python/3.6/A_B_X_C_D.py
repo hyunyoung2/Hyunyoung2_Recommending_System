70a9c858d97165ad4c3689b0f639cb5e2ac81d68
hyunyoung2 <le32146@gmail.com>
2017-06-16 23:25:01 +0900

    Update A_B_X_C_D.py
    currently, I implemented nGram function and the wordcounting function
    the nGram function: by default, five gram 
    the wordcouting function: output is dictionary.

--- Python/3.6/A_B_X_C_D.py
+++ Python/3.6/A_B_X_C_D.py
@@ -31,8 +31,6 @@ def readFile (absPath) :
     
     return listOfStr
 
-# @ function : write wordcounting into file
-
 
 # @ function : remove specailChar 
 # input : a string including specialChar
@@ -44,6 +42,9 @@ def removalOfSpecialChar (inputStr) :
          
     return inputStr
 
+
+# -- natural lanuage processing function -- 
+
 # @ function : tokenizing words into a word and sorting 
 # input : string 
 # output : List of token of words
@@ -52,6 +53,57 @@ def tokenization (wordstring) :
     tempList.sort()
     return tempList
 
+
+# @ function :  how to make nGram like bigram, threegram
+# input : a sorted list  
+# output : nGram list sorted like bigram, threegram 
+def nGram (listOfWord, n = 5) :
+    newWord = [] 
+    # len of List 
+    lenOfList = len(listOfWord)
+    flag = 0
+    # this for statement is zero-based. 
+    for idx, var in enumerate(listOfWord) :
+        tempStr = ""
+        for i in range(0,n):
+            if idx + n > lenOfList :
+                flag = 1
+                break
+            elif i == 0 : 
+                tempStr += var
+            elif i + idx < lenOfList :
+                #print ("no addition into list")
+                #print ("total : %d, current idx : %d" % (lenOfList, (i+idx)))
+                tempStr += " " + listOfWord[idx+i]
+        
+        if flag == 1 :
+            break           
+        newWord.append(tempStr)
+
+    #sorting newWord list ahead of making dictionary of wordcounting
+    #newWord.sort()
+    
+    return newWord
+
+
+# @ function : wordcounting of nGram
+# input : list of nGram
+# ouput : dictionary of wordcounting 
+# my case is also dicionary is sorted.
+def wordCounting (nGram) :
+    nDic = {}
+    
+    for idx, var in enumerate(nGram) :
+        if nDic.get(var) == None : 
+            nDic[var] = 1
+        else :
+            nDic[var] += 1
+
+    return nDic
+
+
+
+
 # @ intial fucntion in if __name__ == "main" :
 # a sequance of this program. 
 def main () :
@@ -59,7 +111,9 @@ def main () :
     timerStr = ["====== recommendation system starts ======\n",
                 "====== readFile function is done :",
                 "====== removal of specialchar function is done :",
-                "====== tokenization function is done :",]
+                "====== tokenization function is done :",
+                "====== nGram is done :",
+                "====== wordcounting is done :"]
     
     print (timerStr[0]) # start of this program
     
@@ -82,9 +136,25 @@ def main () :
     end = time.clock()
     elapsedTime = end - begin 
     print (timerStr[3], elapsedTime, "Seconds ======\n") # end of tokenization
-    print (tempStr2)      
+    #print (tempStr2)  
 
+    begin = time.clock()
+    tempStr3=nGram(tempStr2)
+    end = time.clock()
+    elapsedTime = end - begin 
+    print (timerStr[4], elapsedTime, "Seconds ======\n") # end of wordcouting
+    #print (tempStr3)    
 
+    begin = time.clock()
+    tempStr4=wordCounting(tempStr3)
+    end = time.clock()
+    elapsedTime = end - begin 
+    print (timerStr[5], elapsedTime, "Seconds ======\n") # end of wordcouting
+    #print (tempStr4)  
+    
+    
+    
+    
     
 # @ if statement for execution of this file   
 if __name__ == "__main__" : 
